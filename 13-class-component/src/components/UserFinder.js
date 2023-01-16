780fc8130add0cfc504281bbb366cbf42ffa440a
Bui Nguyen Tung <buinguyentung2308@gmail.com>
2023-01-16 10:49:10 -0600

    13-class-component: convert User, Users to class

---
+++ 13-class-component/src/components/UserFinder.js
@@ -0,0 +1,37 @@
+import { Fragment, useState, useEffect } from 'react';
+
+import Users from './Users';
+import classes from './UserFinder.module.css';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
+const UserFinder = () => {
+  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    console.log('RUN EFFECT ', searchTerm);
+    setFilteredUsers(
+      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+    );
+  }, [searchTerm]);
+
+  const searchChangeHandler = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <Fragment>
+      <div className={classes.finder}>
+        <input type="search" onChange={searchChangeHandler} />
+        <Users users={filteredUsers} />
+      </div>
+    </Fragment>
+  );
+};
+
+export default UserFinder;
